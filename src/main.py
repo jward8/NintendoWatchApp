1c4bff406563cdf6df0233f1e53d69d5ae4f86a0
jackmichael <jward8@wisc.edu>
2020-07-11 11:37:06 -0500

    added the summarization method

--- src/main.py
+++ src/main.py
@@ -10,21 +10,16 @@ class nintendoWatch:
         sleep(2)
 
 
-def send_msg(message,stores, place):
+def send_msg(message):
     account_sid = 'ACf46189a9ceca2b697f802f912661f127'
     auth_token = 'd294e42e19c2fedc65a006946efe8fa7'
     client = Client(account_sid, auth_token)
-    store_msg = place + ", " + message + "\n"
 
-    if len(stores) != 0:
-        for x in stores:
-            store_msg += x['store_name'] + ": " + x['miles'] + "\n"
-
-    message = client.messages \
+    client.messages \
         .create(
-        body=store_msg,
+        body=message,
         from_='+12029521329',
-        to='+14147952004'
+        to='+16085142318'
     )
 
 
@@ -32,34 +27,38 @@ def check_bestBuy(self):
     self.driver.get(
         "https://www.bestbuy.com/site/nintendo-switch-32gb-console-neon-red-neon-blue-joy-con/6364255.p?skuId=6364255")
     best_btn = self.driver.find_element_by_xpath("//div[@class='fulfillment-add-to-cart-button']/div/button")
-    store_pickup = self.driver.find_element_by_xpath("//*[@id='fulfillment-fulfillment-summary-4c1e0ec7-88db-44f0-b3c8-9940cd592454']/div/div/div/div/div[2]/div/span")
 
-    if best_btn.text == 'Sold Out' or "unavailable" in store_pickup.text:
-        send_msg("Sold out at Best Buy")
+    if best_btn.text == 'Sold Out':
+        return False
     else:
-        send_msg("In Stock! Buy Soon!")
+        store_pickup = self.driver.find_element_by_xpath(
+            "//*[@id='fulfillment-fulfillment-summary-4c1e0ec7-88db-44f0-b3c8-9940cd592454']/div/div/div/div/div[2]/div/span")
+        if "unavailable" in store_pickup.text:
+            return False
+        return True
 
 
 def check_target(self):
     ret_list = []
     self.driver.get("https://www.target.com/p/nintendo-switch-with-neon-blue-and-neon-red-joy-con/-/A-77464001")
     sleep(3)
-    self.driver.find_element_by_xpath("//button[@data-test='fiatsButton']")\
+    self.driver.find_element_by_xpath("//button[@data-test='fiatsButton']") \
         .click()
     sleep(1)
-    self.driver.find_element_by_xpath("//a[@class='Link-sc-1khjl8b-0 bTKAgl']")\
+    self.driver.find_element_by_xpath("//a[@class='Link-sc-1khjl8b-0 bTKAgl']") \
         .click()
     sleep(2)
 
-    #sets the location
-    self.driver.find_element_by_xpath("//input[@id='storeSearch']").click()
-    self.driver.find_element_by_xpath("//input[@id='storeSearch']").send_keys(Keys.CONTROL + "a")
-    self.driver.find_element_by_xpath("//input[@id='storeSearch']").send_keys("53715")
+    # sets the location
+    input_location = "//input[@id='storeSearch']"
+    self.driver.find_element_by_xpath(input_location).click()
+    self.driver.find_element_by_xpath(input_location).send_keys(Keys.CONTROL + "a")
+    self.driver.find_element_by_xpath(input_location).send_keys("53715")
 
-    self.driver.find_element_by_xpath("//button[@data-test='fiatsUpdateLocationSubmitButton']")\
+    self.driver.find_element_by_xpath("//button[@data-test='fiatsUpdateLocationSubmitButton']") \
         .click()
     sleep(1)
-    self.driver.find_element_by_class_name('switch-track')\
+    self.driver.find_element_by_class_name('switch-track') \
         .click()
     sleep(1)
     locations = self.driver.find_elements_by_xpath("//div[@data-test='storeAvailabilityStoreCard']")
@@ -73,20 +72,29 @@ def check_target(self):
             }
             ret_list.append(entry)
 
-    if len(ret_list) > 0:
-        send_msg("In Stock at these locations", ret_list, "Target")
+    return ret_list
+
+
+def summarize(self):
+    at_bestBuy = check_bestBuy(self)
+    target_list = check_target(self)
+    at_target = len(target_list) > 0
+
+    summary_msg = "Nintendo Switch Locations: \n"
+    if at_bestBuy:
+        summary_msg += "BestBuy: It's available! Check website! \n"
     else:
-        send_msg("Sold out at Target, closest store is not within 20 miles", [], "Target")
+        summary_msg += "BestBuy: Unavailable :( \n"
 
-    # if int(text[1]) > 20:
-    #     send_msg("Sold out at Target, closest store is " + text[2] + " miles away", [])
-    # else:
-    #     for loc in locations:
-    #         text = loc.text.split()
-    #         if
-    #     send_msg("In Stock! A store in 20 miles has one")
+    if at_target:
+        summary_msg += "Target: It's available at these locations: \n"
+        for x in target_list:
+            summary_msg += x['store_name'] + ": " + x['miles'] + "\n"
+    else:
+        summary_msg += "Target: Unavailable in the nearest 20 miles :("
 
+    send_msg(summary_msg)
 
 
 checker = nintendoWatch()
-check_target(checker)
+summarize(checker)
